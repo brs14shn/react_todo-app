b307ec0c3c075b0baa17eee7839d1cff3e9c69a4
brs14shn <brsshn292929@gmail.com>
2022-07-30 16:06:42 +0300

    App updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
@@ -6,12 +7,20 @@ function App() {
       <div className='row'>
         <div className='col'>
           <h2 className='text-center mb-4'>My Todos</h2>
-        </div>
         <div className='d-flex gap-4 mb-4'>
           <input type="text"
           name='add-todo'
           className='form-control shadow-none text-capitalize'
-          placeholder='Enter new Task' />
+          placeholder='Enter new Task' 
+          // value=""
+          />
+          <button className='btn btn-warning px-3 fw-bold shadow-none'>Add</button>
+          
+          </div>
+          <ul className='list-group'>
+            <Todos/>
+
+          </ul>
         </div>
       </div>
       
