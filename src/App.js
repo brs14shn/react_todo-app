3808e1038042783adcaf039c06a0d02e1911be15
brs14shn <brsshn292929@gmail.com>
2022-07-30 17:12:38 +0300

    updated && localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
@@ -6,12 +6,23 @@ function App() {
 
 const [todo,setTodo] =useState("")
 
+const [allTodos,setallTodos] =useState(JSON.parse(localStorage.getItem("Tasks")))
+
 // console.log(todo);
 //? Add Button
 const handleAddTodo=()=>{
-
-
+  if(todo !==""){
+   setTodo({...allTodos, todo})
+  };
+  setTodo("")  
 }
+
+
+
+useEffect(() => {
+  localStorage.setItem("Tasks",JSON.stringify(allTodos))
+}, [allTodos])
+
 
 
 
@@ -34,6 +45,7 @@ const handleAddTodo=()=>{
           <h2 className='text-center mb-4'>My Todos</h2>
         <div className='d-flex gap-4 mb-4'>
           <input type="text"
+          
           name='add-todo'
           className='form-control shadow-none text-capitalize'
           placeholder='Enter new Task' 
@@ -48,7 +60,6 @@ const handleAddTodo=()=>{
           </div>
           <ul className='list-group'>
             <Todos/>
-
           </ul>
         </div>
       </div>
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 
 const Todos = () => {
   return (
-    <div>Todos</div>
-  )
-}
+    <div className="d-flex justify-content-between align-items-center my-2">
+      <li className="list-group-item border-0 fs-5 text-capitalize">Todos</li>
+      <div className="p-0 m-0 d-flex gap-3">
+        <button className="btn btn-success px-3">
+          <FaEdit size={20} />
+        </button>
+        <button className="btn btn-danger px-3">
+          <AiFillDelete size={22} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Todos
+export default Todos;
